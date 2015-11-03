49a40851bc9280ea8a33d21f7943b51965001505
zhushiyao68@qq.com <zhushiyao68>
2015-11-03 15:33:20 +0800

    zsy:练习封装组件

---
+++ public/script/modules/sea/sea-config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by pcuser on 2015/11/2.
+ */
+
+
+seajs.config({
+    alias : {
+        'jquery' : 'jquery/jquery-2.1.4.js',
+        exports : 'jQuery'
+    }
+});
+
---
+++ public/script/photo/photo.js
@@ -0,0 +1,3 @@
+/**
+ * Created by pcuser on 2015/11/2.
+ */
